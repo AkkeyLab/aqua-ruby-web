122a7e717e71e74b55300bfbc87366f6a108eb3a
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-05-07 00:14:51 +0900

    :sparkles: Update dynamic head title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,25 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @app_name = "aqua.rb"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@app_name}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@app_name}"
   end
 
   test "should get About" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@app_name}"
   end
 end
