2d0c06815acf008d07bc452f7fe3cc29554bc920
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-05-14 01:01:43 +0900

    :sparkles: Create link check test case

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    # Logo-image and Home-text in Header
+    assert_select "a[href=?]", root_path, count: 2
+    # Help-text in Header
+    assert_select "a[href=?]", help_path
+    # About-text in Footer
+    assert_select "a[href=?]", about_path
+    # Contact-text in Footer
+    assert_select "a[href=?]", contact_path
+  end
 end
