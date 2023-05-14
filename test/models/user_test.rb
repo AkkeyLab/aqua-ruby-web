f40aebf07aa91572fcdf77478910eb13dd1c2f06
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-05-14 18:56:43 +0900

    :sparkles: Add validate process for User.name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,12 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: 'akkey', email: 'akkey@rails.rb')
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = ' '
+    assert_not @user.valid?
   end
 end
