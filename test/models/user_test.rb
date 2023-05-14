13a7d8f6eec5e480e3685bd602c9d28aaadb296f
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-05-14 20:13:56 +0900

    :sparkles: Support regular expression check process

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,20 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "#{'A' * 247}@rails.rb"
     assert_not @user.valid?
   end
+
+  test 'email validation should accept valid addresses' do
+    valid_addresses = %w[akkey@rails.rb AKKEY@rails.RB A-K_KEY@rails.co.rb akkey.lab@rails.jp akkey+lab@rails.ai]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject valid addresses' do
+    invalid_addresses = %w[akkey@rails,rb AKKEYrails.RB A-K_KEY@rails.co. akkey.lab@ruby_on_rails.jp akkey+lab@rails+.ai]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
