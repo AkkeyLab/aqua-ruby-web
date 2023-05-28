f722a649adaf0607df946e08400c6ca642baf5ba
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-05-28 22:14:05 +0900

    :sparkles: Add invalid sign-up test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @app_name = 'aqua.rb'
+  end
+
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      user = {
+        name: 'Ai Hoshino',
+        email: 'hoshino@bkomachi.ai',
+        password: 'pass',
+        password_confirmation: 'pass'
+      }
+      post users_path, params: { user: user }
+    end
+
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+    assert_select 'title', "Sign Up | #{@app_name}"
+  end
 end
