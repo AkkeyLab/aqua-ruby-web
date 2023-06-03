908d9f4ecccb10c3f5be37d26f348bc676e467ee
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-06-04 01:18:04 +0900

    :sparkles: Add create and destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    is_auth_user = user.authenticate(params[:session][:password])
+    if user && is_auth_user
+      # TODO: Show redirect to user page
+    else
+      # TODO: Show error message
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
