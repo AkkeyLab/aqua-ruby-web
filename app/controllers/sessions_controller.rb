f015718f1fe5ae6d7a1bd2602764523964221f37
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-06-04 16:58:06 +0900

    :sparkles: Add unit test and fix some bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    is_auth_user = user.authenticate(params[:session][:password])
-    if user && is_auth_user
+    if user && user.authenticate(params[:session][:password])
       # TODO: Show redirect to user page
     else
-      flash[:danger] = 'Invalid email or password, combination'
+      flash.now[:danger] = 'Invalid email or password, combination'
       render 'new', status: :unprocessable_entity
     end
   end
