f825f31086a0563fcf53eb49c8142ca165ad05d4
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-06-04 22:02:11 +0900

    :sparkles: Add log-out process

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -20,6 +20,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: @user.email, password: ''} }
+    assert_not is_logged_in?
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
@@ -31,11 +32,28 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: @user.email, password: 'password'} }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path, count: 1
     assert_select "a[href=?]", user_path(@user), count: 1
+  end
+
+  test 'logout user' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: @user.email, password: 'password'} }
+    assert is_logged_in?
+    follow_redirect!
+    delete logout_path
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 1
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
