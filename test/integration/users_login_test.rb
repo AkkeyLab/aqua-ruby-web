4dbb2011ae0c4e3f6ee006b28c39b09bc3086482
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-06-04 19:57:40 +0900

    :sparkles: Add test case

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,6 +16,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
+  test 'login with invalid password' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: @user.email, password: ''} }
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
   test 'login with valid information' do
     get login_path
     assert_template 'sessions/new'
