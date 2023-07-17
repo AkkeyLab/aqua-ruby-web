2532a99e7d985c9e96ca3e8a42dbb1c7138e03fb
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-07-17 17:38:47 +0900

    :sparkles: Add user edit test case

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:akkey)
+  end
+
+  test 'unsuccessful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    param = { name: '', email: '', password: '', password_confirmation: '' }
+    patch user_path(@user), params: { user: param }
+    assert_template 'users/edit'
+  end
 end
